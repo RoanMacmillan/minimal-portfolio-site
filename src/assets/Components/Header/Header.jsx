455a7317833dad0724f07b4246ee756204ac38ec
RoanMacmillan <hibee94@gmail.com>
2023-03-30 16:03:24 +0100

    fixed some html errors

--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -6,11 +6,10 @@ import Icon from "../Icons/Icon";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Header = () => {
-
   const navigate = useNavigate();
   const location = useLocation();
 
-  const isActive = (path) => location.pathname === path ? 'activeLink' : '';
+  const isActive = (path) => (location.pathname === path ? "activeLink" : "");
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const headerRef = useRef(null);
@@ -40,47 +39,46 @@ const Header = () => {
             src={CloseBtn}
             alt="Close button"
             onClick={() => setIsMobileMenuOpen(false)}
-            className='hamburger'
+            className="hamburger"
           />
         ) : (
           <img
             src={Hamburger}
             alt="Hamburger"
             onClick={() => setIsMobileMenuOpen(true)}
-            className='hamburger'
-
+            className="hamburger"
           />
         )}
 
         <div
           className={`mobileMenu ${isMobileMenuOpen ? "active" : "inactive"}`}
         >
-          <ul className="mobileLinks">
-            <Link to="/">
-              <li>Home</li>
-            </Link>
+          <div className="mobileLinks">
+            <Link to="/">Home</Link>
 
-            <Link to='/index'>
-            <li>Portfolio</li>
-            </Link>
-            <Link to='/contact-me'>
-            <li>Contact</li>
-            </Link>
-          </ul>
+            <Link to="/index">Portfolio</Link>
+            <Link to="/contact-me">Contact</Link>
+          </div>
         </div>
         <div className="desktopNav">
-  <ul className="desktopLinks">
-    <li className={isActive('/')} onClick={() => navigate('/')}>
-      Home
-    </li>
-    <li className={isActive('/index')} onClick={() => navigate('/index')}>
-      Portfolio
-    </li>
-    <li className={isActive('/contact-me')} onClick={() => navigate('/contact-me')}>
-      Contact me
-    </li>
-  </ul>
-</div>
+          <ul className="desktopLinks">
+            <li className={isActive("/")} onClick={() => navigate("/")}>
+              Home
+            </li>
+            <li
+              className={isActive("/index")}
+              onClick={() => navigate("/index")}
+            >
+              Portfolio
+            </li>
+            <li
+              className={isActive("/contact-me")}
+              onClick={() => navigate("/contact-me")}
+            >
+              Contact me
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
